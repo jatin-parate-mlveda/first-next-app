aebd0bc59619f19204bf65fad721b62bb72cdbfd
Jatin <jatin.parate@mlveda.com>
2023-03-04 13:55:03 +0530

    trying to fix something

--- next.config.js
+++ next.config.js
@@ -1,22 +1,28 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
-        source: '/apps/:path',
-        destination: '/apps/:path',
+        source: "/_next/:path*",
+        destination: "/_next/:path*",
       },
       {
-        source: '/_next/:path',
-        destination: '/_next/:path'
+        source: "/apps/:path*",
+        destination: "/apps/:path*",
       },
       {
-        source: '/:path',
-        destination: 'http://45.79.140.122/:path',
-      }
-    ]
-  }
-}
+        source: "/:path*",
+        destination: "http://45.79.140.122/:path*",
+        // middleware: createProxyMiddleware({
+        //   target: "http://45.79.140.122",
+        //   changeOrigin: false,
+        // }),
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
