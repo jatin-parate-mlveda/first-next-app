68137c124fca70d7dd11062f478a10cc80202c12
Jatin <jatin.parate@mlveda.com>
2023-03-04 22:21:39 +0530

    Updated redirects

--- next.config.js
+++ next.config.js
@@ -1,53 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   async rewrites() {
     return {
-      // beforeFiles: [
-      //   {
-      //     source: "/_next/:path*",
-      //     destination: "/_next/:path*",
-      //   },
-      //   {
-      //     source: "/apps/",
-      //     destination: "/apps/",
-      //   },
-      //   {
-      //     source: "/apps/:path*",
-      //     destination: "/apps/",
-      //   },
-      //   // {
-      //   //   source: "/:path*",
-      //   //   destination: "/api/proxy/",
-      //   // },
-      // ],
       afterFiles: [
+        // {
+        //   source: "/_next/:path*",
+        //   destination: "/_next/:path*",
+        // },
+        // {
+        //   source: "/apps/",
+        //   destination: "/apps/",
+        // },
+        // {
+        //   source: "/apps/:path*",
+        //   destination: "/apps/:path*",
+        // },
         {
-          source: "/apps/:path*",
-          destination: "/apps/:path*",
+          source: "/(!apps)?:path*/",
+          destination: "http://45.79.140.122/:path*/",
         },
+        // {
+        //   source: '/:path/',
+        //   destination: "http://45.79.140.122/:path/",
+        // },
         {
-          source: "/:path*",
-          destination: "/api/proxy/",
-        },
-        {
-          source: "/(!apps)?:path*",
-          destination: "/api/proxy/",
-        },
+          source: '/:path/',
+          destination: "http://45.79.140.122/:path/",
+        }
       ],
+      // afterFiles: [
+      //   // {
+      //   //   source: "/(!apps)?:path*",
+      //   //   destination: "http://45.79.140.122/:path*",
+      //   // },
+      //   {
+      //     source: "/:path*",
+      //     destination: "http://45.79.140.122/:path*",
+      //   },
+      // ],
     };
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       destination: "/api/proxy/:path*",
-  //     },
-  //   ];
-  // },
-  // async serverRuntimeConfig() {
-  //   return {};
-  // },
 };
 
 module.exports = nextConfig;
