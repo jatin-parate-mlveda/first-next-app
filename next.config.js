fbb69ce5b44dcc265588a6ebf6069ce7f7f924c8
Jatin <jatin.parate@mlveda.com>
2023-03-04 20:12:28 +0530

    added redirections

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://45.79.140.122/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
