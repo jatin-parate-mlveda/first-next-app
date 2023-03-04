7dbe43968d90eb54266f368a77f2ef9c7bedcd92
Jatin <jatin.parate@mlveda.com>
2023-03-04 14:22:17 +0530

    Revert "trying something"
    This reverts commit bd60a9c57d8a68076a4d088d603f8e278a34e074.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: "/_next/:path*",
+        destination: "/_next/:path*",
+      },
+      {
+        source: "/apps/",
+        destination: "/apps/",
+      },
+      {
+        source: "/apps/:path*",
+        destination: "/apps/:path*",
+      },
+      {
         source: "/:path*",
         destination: "http://45.79.140.122/:path*",
         // middleware: createProxyMiddleware({
