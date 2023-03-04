f951ebe62847fc1b478f085984305a0d68b8602a
Jatin <jatin.parate@mlveda.com>
2023-03-04 20:25:30 +0530

    trying rewrites

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: "/_next/:path*",
+        destination: "/_next/:path*",
+      },
+      {
+        source: "/apps/",
+        destination: "/apps/",
+      },
+      {
+        source: "/apps/:path*",
+        destination: "/apps/:path*",
+      },
+      {
         source: "/:path*",
         destination: "http://45.79.140.122/:path*",
       },
