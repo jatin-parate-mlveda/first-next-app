e13afbfde1760bb49e1bf052aa3537e6504e4c6c
Jatin <jatin.parate@mlveda.com>
2023-03-04 13:24:49 +0530

    fix issue

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/apps*',
-        destination: '/apps*',
+        source: '/apps/:path',
+        destination: '/apps/:path',
       },
       {
-        source: '/_next*',
-        destination: '/_next*'
+        source: '/_next/:path',
+        destination: '/_next/:path'
       },
       {
-        source: '/*',
-        destination: 'http://45.79.140.122/*',
+        source: '/:path',
+        destination: 'http://45.79.140.122/:path',
       }
     ]
   }
