4310b190bb8be29b6fce7d714ba9d6872f00a02c
Jatin <jatin.parate@mlveda.com>
2023-03-04 13:20:15 +0530

    rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/apps*',
+        destination: '/apps*',
+      },
+      {
+        source: '/_next*',
+        destination: '/_next*'
+      },
+      {
+        source: '/*',
+        destination: 'http://45.79.140.122/*',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
