be8783acf1e5adbb0604c3cffbfa89d3e61e7735
Jatin <jatin.parate@mlveda.com>
2023-03-11 23:31:19 +0530

    Fix redirections

--- next.config.js
+++ next.config.js
@@ -20,10 +20,62 @@ const nextConfig = {
           destination: "http://45.79.140.122/wp-content/:path*",
         },
         {
+          source: "/(!apps)?wp-content/:path*/",
+          destination: "http://45.79.140.122/wp-content/:path*/",
+        },
+        {
           source: "/superadmin/:path*",
           destination: "http://45.79.140.122/superadmin/:path*",
         },
+        {
+          source: "/(!apps)?superadmin/:path*/",
+          destination: "http://45.79.140.122/superadmin/:path*/",
+        },
       ],
+      afterFiles: [
+        // {
+        //   source: "/_next/:path*",
+        //   destination: "/_next/:path*",
+        // },
+        // {
+        //   source: "/apps/",
+        //   destination: "/apps/",
+        // },
+        // {
+        //   source: "/apps/:path*",
+        //   destination: "/apps/:path*",
+        // },
+        // {
+        //   source: "/(!apps)?wp-content/:path/",
+        //   destination: "http://45.79.140.122/wp-content/:path/",
+        // },
+        {
+          source: "/(!apps)?superadmin/:path/",
+          destination: "http://45.79.140.122/superadmin/:path/",
+        },
+        // {
+        //   source: "/(!apps)?:path*/",
+        //   destination: "http://45.79.140.122/:path*/",
+        // },
+        // {
+        //   source: '/:path/',
+        //   destination: "http://45.79.140.122/:path/",
+        // },
+        // {
+        //   source: "/:path/",
+        //   destination: "http://45.79.140.122/:path/",
+        // },
+      ],
+      // afterFiles: [
+      //   // {
+      //   //   source: "/(!apps)?:path*",
+      //   //   destination: "http://45.79.140.122/:path*",
+      //   // },
+      //   {
+      //     source: "/:path*",
+      //     destination: "http://45.79.140.122/:path*",
+      //   },
+      // ],
     };
   },
 };
