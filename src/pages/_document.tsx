b1f034b3176d5940e482bc3b925515200c17e2ea
Jatin <jatin.parate@mlveda.com>
2023-03-11 22:40:25 +0530

    Fix console errors and load css without domain prefix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* eslint-disable-next-line @next/next/no-css-tags */}
         <link
           rel="stylesheet"
           id="spdigest-style-css"
-          href="https://www.shopdigest.com/wp-content/themes/spdigest/style.css"
+          href="/wp-content/themes/spdigest/style.css"
+          // href="http://45.79.140.122/wp-content/themes/spdigest/style.css"
           media="all"
         />
       </Head>
